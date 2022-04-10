de3288d7507bd782e39338cf07206b9eeb00759a
WakelessSloth56 <wsloth56@foxmail.com>
2022-04-10 23:23:38 +0800

    feat(affix): sticky default settings
    close #6

--- src/main/java/org/auioc/mcmod/extrachampions/common/affix/impl/StickyAffix.java
+++ src/main/java/org/auioc/mcmod/extrachampions/common/affix/impl/StickyAffix.java
@@ -1,20 +1,22 @@
 package org.auioc.mcmod.extrachampions.common.affix.impl;
 
 import java.util.function.IntUnaryOperator;
+import org.auioc.mcmod.extrachampions.api.affix.AffixBasicConfig;
+import org.auioc.mcmod.extrachampions.api.affix.EmptyAffixExtraConfig;
+import org.auioc.mcmod.extrachampions.api.affix.ExtraAffix;
 import org.auioc.mcmod.extrachampions.utils.ChampionHelper;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import top.theillusivec4.champions.api.AffixCategory;
 import top.theillusivec4.champions.api.IChampion;
-import top.theillusivec4.champions.common.affix.core.BasicAffix;
 
-public class StickyAffix extends BasicAffix {
+public class StickyAffix extends ExtraAffix<EmptyAffixExtraConfig> {
 
-    private static final IntUnaryOperator CHANCE = (tier) -> 50 + tier * 10;
+    private static final IntUnaryOperator CHANCE = (tier) -> 30 + tier * 10;
 
     public StickyAffix() {
-        super("sticky", AffixCategory.OFFENSE);
+        super("sticky", AffixCategory.OFFENSE, () -> new AffixBasicConfig().setMinTier(2), EmptyAffixExtraConfig::new);
     }
 
     @Override
